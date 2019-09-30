3231ede03edf50125ce035f80ee0041276c6b79d
Casey Wylie <cwylie@redhat.com>
2019-09-30 16:59:34 -0400

    Created component SignUpForAccount Messabge so LandingContainer is componetized

---
+++ src/components/SignUpForAccountMessage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { LoginMainFooterBandItem } from '@patternfly/react-core';
+
+const SignUpForAccountMessage = props =>
+<LoginMainFooterBandItem>
+Need an account? 
+<a onClick={props.handleClick}>Sign up.</a>
+</LoginMainFooterBandItem>
+  
+
+export default SignUpForAccountMessage
