46482e1e37be749f14c622710c58084b9d4f3f54
Casey Wylie <cwylie@redhat.com>
2019-09-30 16:39:57 -0400

    Added UserProvider, UserConsumer to reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { useHistory, RouterProvider, useRouter } from './RouterReducer'
 import { useTheme, ThemeConsumer, ThemeProvider } from './ThemeReducer'
-
+import { UserProvider, UserConsumer } from './UserReducer'
 export {
     useHistory,
     RouterProvider,
     useRouter,
     useTheme,
     ThemeConsumer,
-    ThemeProvider
+    ThemeProvider,
+    UserProvider,
+    UserConsumer
 }
