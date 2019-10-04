67b110f8d3b27f7b6734e7564839b4c35093f3d1
Casey Wylie <cwylie@redhat.com>
2019-10-04 16:51:45 -0400

    create read,set,get cookie functions

---
+++ src/libs/cookie-lib.js
@@ -0,0 +1,11 @@
+import Cookies from 'js-cookie'
+
+export const readCookies = () => Cookies.get()
+
+export const setCookie = (name, value, expires ) => {
+    Cookies.set(name, value, { expires })
+}
+
+export const getCookie = name => {
+    Cookies.get(name)
+}
