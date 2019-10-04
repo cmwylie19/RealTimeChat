338f3bde3d7436a010435163fc335e178cfa9d12
Casey Wylie <cwylie@redhat.com>
2019-10-04 16:07:32 -0400

    added UserReducer and create userLogin and userLogout actions and action cases in reducer

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,15 +1,15 @@
 import React, { useReducer, createContext, useContext } from 'react'
-import { UserLogo } from '../assets/images' 
+import { UserLogo } from '../assets/images'
 var initialState = {
     given_name: "",
     family_name: "",
     preferred_username: "",
     name: "",
-    refreshToken:"",
-    idToken:"",
-    token:"",
-    accountRoles:"",
-    realmAccessRoles:"",
+    refreshToken: "",
+    idToken: "",
+    token: "",
+    accountRoles: [],
+    realmAccessRoles: [],
     email: "",
     remember: false,
     avatar: UserLogo,
@@ -20,6 +20,34 @@ var initialState = {
 
 const UserReducer = (state = {}, action) => {
     switch (action.type) {
+        case "USER_LOGIN":
+            return {
+                ...state,
+                given_name: action.payload.given_name,
+                family_name: action.payload.family_name,
+                preferred_username: action.payload.preferred_username,
+                name: action.payload.name,
+                refreshToken: action.payload.refreshToken,
+                idToken: action.payload.idToken,
+                token: action.payload.token,
+                accountRoles: action.payload.accountRoles,
+                realmAccessRoles: action.payload.realmAccessRoles,
+                email: action.payload.email
+            }
+        case "USER_LOGOUT":
+            return {
+                ...state,
+                given_name: null,
+                family_name: null,
+                preferred_username: null,
+                name: null,
+                refreshToken: null,
+                idToken: null,
+                token: null,
+                accountRoles: null,
+                realmAccessRoles: null,
+                email: null
+            }
         case "SET_GIVEN_NAME":
             return { ...state, given_name: action.payload }
         case "SET_FAMILY_NAME":
@@ -48,13 +76,38 @@ export const UserProvider = ({ children }) => {
         email: state.email,
         password: state.password,
         remember: state.remember,
-        setFirst: given_name=> dispatch({type:"SET_GIVEN_NAME", payload: given_name}),
-        setLast: family_name => dispatch({ type: "SET_FAMILY_NAME", payload: family_name}),
-        setRemember: remember=> dispatch({type:"SET_REMEMBER", payload: remember}),
-        setEmail: email => dispatch({ type: "SET_EMAIL", payload: email}),
-        setPassword: password => dispatch({ type: "SET_PASSWORD", payload: password}),
-        setToken: token=> dispatch({type:"SET_TOKEN", payload: token}),
-        setAvatar: avatar => dispatch({ type: "SET_AVATAR", payload: avatar})
+        userLogin: (given_name,
+            family_name,
+            preferred_username,
+            name,
+            refreshToken,
+            idToken,
+            token,
+            accountRoles,
+            realmAccessRoles,
+            email) => dispatch({
+                type: "USER_LOGIN", 
+                payload: {
+                    given_name,
+                    family_name,
+                    preferred_username,
+                    name,
+                    refreshToken,
+                    idToken,
+                    token,
+                    accountRoles,
+                    realmAccessRoles,
+                    email
+                }
+            }),
+        userLogout: ()=> dispatch({type: "USER_LOGOUT"}),
+        setFirst: given_name => dispatch({ type: "SET_GIVEN_NAME", payload: given_name }),
+        setLast: family_name => dispatch({ type: "SET_FAMILY_NAME", payload: family_name }),
+        setRemember: remember => dispatch({ type: "SET_REMEMBER", payload: remember }),
+        setEmail: email => dispatch({ type: "SET_EMAIL", payload: email }),
+        setPassword: password => dispatch({ type: "SET_PASSWORD", payload: password }),
+        setToken: token => dispatch({ type: "SET_TOKEN", payload: token }),
+        setAvatar: avatar => dispatch({ type: "SET_AVATAR", payload: avatar })
     }}>
         {children}
     </UserContext.Provider>
