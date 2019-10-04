086f75fb802fd6cdc39b67c6a605f5f76b944598
Casey Wylie <cwylie@redhat.com>
2019-10-04 16:52:03 -0400

    added functions to index file for libs

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,8 +1,12 @@
 import { success, failure } from './response-lib'
 import { setStorage, getStorage } from './storage-lib'
 import { fetchInstance, instance, headerRequest, authRequest } from './request-lib'
+import { readCookies, getCookie, setCookie } from './cookie-lib'
 export {
     setStorage,
+    readCookies,
+    setCookie,
+    getCookie,
     getStorage,
     success,
     failure,
