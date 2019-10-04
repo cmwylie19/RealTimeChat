5687e7e917634c04ab30a7f6d53c6d0375b79bb1
Casey Wylie <cwylie@redhat.com>
2019-10-04 15:54:56 -0400

    latest updates

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,8 +1,8 @@
 import React, { useReducer, createContext, useContext } from 'react'
 import { UserLogo } from '../assets/images' 
 var initialState = {
-    first: "",
-    last: "",
+    given_name: "",
+    family_name: "",
     email: "",
     token: "",
     remember: false,
@@ -14,10 +14,10 @@ var initialState = {
 
 const UserReducer = (state = {}, action) => {
     switch (action.type) {
-        case "SET_FIRST":
-            return { ...state, first: action.payload }
-        case "SET_LAST":
-            return { ...state, last: action.payload }
+        case "SET_GIVEN_NAME":
+            return { ...state, given_name: action.payload }
+        case "SET_FAMILY_NAME":
+            return { ...state, family_name: action.payload }
         case "SET_EMAIL":
             return { ...state, email: action.payload }
         case "SET_REMEMBER":
@@ -37,13 +37,13 @@ export const UserProvider = ({ children }) => {
     return <UserContext.Provider value={{
         user: state,
         avatar: state.avatar,
-        first: state.first,
-        last: state.last,
+        given_name: state.given_name,
+        family_name: state.family_name,
         email: state.email,
         password: state.password,
         remember: state.remember,
-        setFirst: first=> dispatch({type:"SET_FIRST", payload: first}),
-        setLast: last => dispatch({ type: "SET_LAST", payload: last}),
+        setFirst: given_name=> dispatch({type:"SET_GIVEN_NAME", payload: given_name}),
+        setLast: family_name => dispatch({ type: "SET_FAMILY_NAME", payload: family_name}),
         setRemember: remember=> dispatch({type:"SET_REMEMBER", payload: remember}),
         setEmail: email => dispatch({ type: "SET_EMAIL", payload: email}),
         setPassword: password => dispatch({ type: "SET_PASSWORD", payload: password}),
