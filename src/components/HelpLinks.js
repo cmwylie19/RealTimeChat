636631b8c4583a5be0fa541f42414b45e82445bf
Casey Wylie <cwylie@redhat.com>
2019-10-06 09:33:44 -0400

    Reformat code

--- src/components/HelpLinks.js
+++ src/components/HelpLinks.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react'
-import { ListItem,LoginFooterItem } from '@patternfly/react-core'
-  const HelpLinks = (
-      <Fragment>
-        <ListItem>
-          <LoginFooterItem href="#">Terms of Use </LoginFooterItem>
-        </ListItem>
-        <ListItem>
-          <LoginFooterItem href="#">Help</LoginFooterItem>
-        </ListItem>
-        <ListItem>
-          <LoginFooterItem href="#">Privacy Policy</LoginFooterItem>
-        </ListItem>
-      </Fragment>
-    );
-export default HelpLinks
+import { ListItem, LoginFooterItem } from '@patternfly/react-core'
+
+export const HelpLinks = (
+  <Fragment>
+    <ListItem>
+      <LoginFooterItem href="#">Terms of Use </LoginFooterItem>
+    </ListItem>
+    <ListItem>
+      <LoginFooterItem href="#">Help</LoginFooterItem>
+    </ListItem>
+    <ListItem>
+      <LoginFooterItem href="#">Privacy Policy</LoginFooterItem>
+    </ListItem>
+  </Fragment>
+);
