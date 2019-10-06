23da552aec453b49cba552c5d36262c0f5c2e4d2
Casey Wylie <cwylie@redhat.com>
2019-10-06 09:43:13 -0400

    documentation and formatting

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,28 +1,53 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Level, LevelItem } from '@patternfly/react-core';
 
 
-const Message = ({type, body, primary}) => {
+const Message = ({ type, body, primary }) => {
     const messageStyle = {
         container: {
             padding: '15px',
             margin: '15px'
         },
-        received:{
+        received: {
             backgroundColor: 'grey',
             borderRadius: '15px',
             padding: '15px'
         },
-        sent:{
+        sent: {
             backgroundColor: primary,
             borderRadius: '15px',
             padding: '15px'
         }
-    } 
-   return  <Level style={messageStyle.container}>
-       {type === "received" ?  <LevelItem style={messageStyle.received}>{body}</LevelItem> : <LevelItem></LevelItem>}
-        <LevelItem ></LevelItem>
-        {type === "sent" ?  <LevelItem style={messageStyle.sent}>{body}</LevelItem> : <LevelItem></LevelItem>}
-    </Level>
+    }
+    return (
+        <Level style={messageStyle.container}>
+
+            {/* 
+            Message Container:
+
+            If message is of designated type, return message with formatting for that type.
+            If message is not of that type, leave space as a place holder.
+            
+            This gives separation between sent and received messages.
+            
+            */}
+
+            {
+                type === "received" ?
+                    <LevelItem style={messageStyle.received}>{body}</LevelItem> :
+                    <LevelItem></LevelItem>
+            }
+
+            {/* Empty middle cell for spacing */}
+            <LevelItem ></LevelItem>
+
+            {/* Messages of type sent */}
+            {
+                type === "sent" ?
+                    <LevelItem style={messageStyle.sent}>{body}</LevelItem> :
+                    <LevelItem></LevelItem>
+            }
+        </Level>
+    )
 }
 export default Message;
