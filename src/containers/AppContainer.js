fe83b1e2e201e43accd4731c8c319e8baa0a5930
cmwylie19 <cwylie@redhat.com>
2019-10-07 13:08:44 -0400

    update to realm export

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { RouterProvider, SocketProvider, ThemeProvider, UserProvider } from '../reducers'
 import { Route } from 'react-router-dom'
 import DashboardContainer from './DashboardContainer'
-// import NavContainer from './NavContainer'
-// import OldNavContainer from './OldNavContainer'
+import NavContainer from './NavContainer'
+import OldNavContainer from './OldNavContainer'
 
 const AppContainer = () => (
     <RouterProvider>
         <ThemeProvider>
             <UserProvider>
                 <SocketProvider>
-                    <Route component={DashboardContainer} path="/" />
-                    {/* <Route component={OldNavContainer} exact path="/prev" />
-                    <Route component={NavContainer} exact path="/nav" /> */}
+                    <Route component={DashboardContainer} exact path="/" />
+                    <Route component={OldNavContainer} exact path="/prev" />
+                    <Route component={NavContainer} exact path="/nav" />
                 </SocketProvider>
             </UserProvider>
         </ThemeProvider>
