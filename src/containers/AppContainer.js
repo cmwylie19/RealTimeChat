7394433939d3fa35604320be185533050e0308a5
cmwylie19 <cwylie@redhat.com>
2019-10-07 17:03:09 -0400

    updates for images

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 import DashboardContainer from './DashboardContainer'
 import NavContainer from './NavContainer'
 import OldNavContainer from './OldNavContainer'
+import LandingPage from './LandingContainer'
 
 const AppContainer = () => (
     <RouterProvider>
@@ -13,6 +14,7 @@ const AppContainer = () => (
                     <Route component={DashboardContainer} exact path="/" />
                     <Route component={OldNavContainer} exact path="/prev" />
                     <Route component={NavContainer} exact path="/nav" />
+                    <Route component={LandingPage} exact path="/landing" />
                 </SocketProvider>
             </UserProvider>
         </ThemeProvider>
