df93f676d10ce57d027078d63af7b3bd7122efba
cmwylie19 <cwylie@redhat.com>
2019-10-22 08:58:40 -0400

    update

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { RouterProvider, SocketProvider, ThemeProvider, UserProvider } from '../reducers'
 import { Route } from 'react-router-dom'
 import DashboardContainer from './DashboardContainer'
-import NavContainer from './NavContainer'
-import OldNavContainer from './OldNavContainer'
-import LandingPage from './LandingContainer'
+
 
 const AppContainer = () => (
     <RouterProvider>
@@ -12,9 +10,6 @@ const AppContainer = () => (
             <UserProvider>
                 <SocketProvider>
                     <Route component={DashboardContainer} exact path="/" />
-                    <Route component={OldNavContainer} exact path="/prev" />
-                    <Route component={NavContainer} exact path="/nav" />
-                    <Route component={LandingPage} exact path="/landing" />
                 </SocketProvider>
             </UserProvider>
         </ThemeProvider>
